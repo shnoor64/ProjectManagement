9bbc6bbcd3044f7aa4b03fbdb58eba099387ca8f
shnoor64 <shnooor64@gmail.com>
2021-07-16 23:41:57 +0300

    Update ProjectRequestDto

--- src/main/java/com/simbirsoft/belousov/rest/dto/ProjectRequestDto.java
+++ src/main/java/com/simbirsoft/belousov/rest/dto/ProjectRequestDto.java
@@ -16,4 +16,43 @@ public class ProjectRequestDto {
 
     @Schema(description = "Статус проекта")
     private String statusProject;
+
+    public ProjectRequestDto(String name, String descriptionProject, String customer, String statusProject) {
+        this.name = name;
+        this.descriptionProject = descriptionProject;
+        this.customer = customer;
+        this.statusProject = statusProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptionProject() {
+        return descriptionProject;
+    }
+
+    public void setDescriptionProject(String descriptionProject) {
+        this.descriptionProject = descriptionProject;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getStatusProject() {
+        return statusProject;
+    }
+
+    public void setStatusProject(String statusProject) {
+        this.statusProject = statusProject;
+    }
 }
