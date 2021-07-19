ae59eab034ebb3828429c49b9b857560fa18fe54
shnoor64 <shnooor64@gmail.com>
2021-07-19 22:58:06 +0300

    Add ErrorController

---
+++ src/main/java/com/simbirsoft/belousov/rest/ErrorController.java
@@ -0,0 +1,18 @@
+package com.simbirsoft.belousov.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity handleIOException(IOException e) {
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
