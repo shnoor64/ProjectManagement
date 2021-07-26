b21a0a3a48bb152150dabe5d7fbc8ba50c533745
shnoor64 <shnooor64@gmail.com>
2021-07-26 17:53:13 +0300

    add repository

---
+++ src/main/java/com/simbirsoft/belousov/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.belousov.repository;
+
+import com.simbirsoft.belousov.entity.ProjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Integer> {
+    Optional<ProjectEntity> findByName();
+}
