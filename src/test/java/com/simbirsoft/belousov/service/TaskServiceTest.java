e159c8d670d0c965a7f579084e80ad3c062a4557
shnoor64 <shnooor64@gmail.com>
2021-08-23 14:40:03 +0300

    fix TaskServiceTest

--- src/test/java/com/simbirsoft/belousov/service/TaskServiceTest.java
+++ src/test/java/com/simbirsoft/belousov/service/TaskServiceTest.java
@@ -1,10 +1,7 @@
 package com.simbirsoft.belousov.service;
 
 import com.simbirsoft.belousov.ProjectManagerApplication;
-import com.simbirsoft.belousov.entity.ProjectEntity;
-import com.simbirsoft.belousov.entity.ReleaseEntity;
-import com.simbirsoft.belousov.entity.TaskEntity;
-import com.simbirsoft.belousov.entity.UserEntity;
+import com.simbirsoft.belousov.entity.*;
 import com.simbirsoft.belousov.enums.StatusPay;
 import com.simbirsoft.belousov.enums.StatusProject;
 import com.simbirsoft.belousov.enums.StatusTask;
@@ -15,8 +12,9 @@ import com.simbirsoft.belousov.repository.UserRepository;
 import com.simbirsoft.belousov.rest.dto.TaskResponseDto;
 import com.simbirsoft.belousov.servise.TaskService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -27,10 +25,9 @@ import java.time.LocalDateTime;
         classes = ProjectManagerApplication.class)
 @TestPropertySource(
         locations = "classpath:application-integrationtest.properties")
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TaskServiceTest {
-
-
+    private RoleEntity roleEntity;
     private ReleaseEntity releaseEntity;
     private ProjectEntity projectEntity;
     private UserEntity userEntity;
@@ -38,19 +35,25 @@ public class TaskServiceTest {
 
     @Autowired
     private TaskRepository taskRepository;
+    @Autowired
     private ProjectRepository projectRepository;
+    @Autowired
     private UserRepository userRepository;
+    @Autowired
     private ReleaseRepository releaseRepository;
+    @Autowired
     private TaskService taskService;
 
 
-    @BeforeEach
+    @BeforeAll
+
     void prepare() {
+        roleEntity = new RoleEntity(1,"oleg");
         releaseEntity = new ReleaseEntity(1, 1, LocalDateTime.of(2021, 8, 8, 12, 0),
                 LocalDateTime.of(2022, 8, 8, 14, 0));
-        projectEntity = new ProjectEntity(1, "Velodrom", "For velodrom", "OOO Velodrom", StatusProject.BACKLOG, StatusPay.PAID);
-        userEntity = new UserEntity(1, "Oleg", "Olegov", "password", null);
-        taskEntity = new TaskEntity(1, "velo", "add velo", projectEntity, StatusTask.DONE, userEntity, userEntity, releaseEntity, 2,
+        projectEntity = new ProjectEntity(1, "Velodrom", "For velodrom", "OOO Velodrom", StatusProject.IN_PROGRESS, StatusPay.PAID);
+        userEntity = new UserEntity(1, "Oleg", "Olegov", "password", roleEntity);
+        taskEntity = new TaskEntity(1, "velo", "add velo", projectEntity, StatusTask.IN_PROGRESS, userEntity, userEntity, releaseEntity, 2,
                 LocalDateTime.of(2021, 8, 8, 12, 0),
                 LocalDateTime.of(2021, 8, 8, 14, 0));
         taskRepository.save(taskEntity);
@@ -59,17 +62,18 @@ public class TaskServiceTest {
     @Test
     void InProgressStatusOfTheStartProject() {
 
+
         TaskResponseDto taskResponseDto = taskService.updateStatusTask(1, "IN_PROGRESS");
 
-        Assertions.assertEquals(taskEntity.getStatusTask(), taskResponseDto.getStatusTask());
+        Assertions.assertEquals(StatusTask.IN_PROGRESS, taskResponseDto.getStatusTask());
     }
 
     @Test
     void DoneStatusOfTheOldStatusInProgress() {
 
-        TaskResponseDto taskResponseDto = taskService.updateStatusTask(1, "CLOSED");
+        TaskResponseDto taskResponseDto = taskService.updateStatusTask(1, "DONE");
 
-        Assertions.assertEquals(taskEntity.getStatusTask(), taskResponseDto.getStatusTask());
+        Assertions.assertEquals(StatusTask.DONE, taskResponseDto.getStatusTask());
     }
 
     @Test
@@ -86,8 +90,4 @@ public class TaskServiceTest {
 
         Assertions.assertTrue(numberOutstandingTask == 1);
     }
-//    @Test
-//    void rerrew() {
-//        Assertions.assertTrue(true);
-//    }
 }
